ebdb7533157df4ecc65e35e9b2ca3b867664896e
avtop3 <avtop25495@gmail.com>
2016-04-16 19:14:37 +0300

    Добавил JS обработку формы регистрации на конференцию

--- common/models/Member.php
+++ common/models/Member.php
@@ -8,7 +8,7 @@
 
 namespace common\models;
 
-
+use Yii;
 use yii\db\ActiveRecord;
 use yii\behaviors\TimestampBehavior;
 use yii\helpers\ArrayHelper;
@@ -16,13 +16,11 @@ use common\models\Countries;
 
 class Member extends ActiveRecord
 {
-    public static $participationTypeVariants = ['listener' => 'As listener', 'speaker' => 'As speaker'];
-    public static $scienceDegreeVariants = [0 => 'No degree', 1 => 'Doctor', 2 => 'Candidate'];
-    public static $scienceTitleVariants = [0 => 'No rank', 1 => 'Full professor', 2 => 'Associate Professor'];
-    public static $organisationActivityVariants = [0 => 'Teaching', 1 => 'Business planning', 2 => 'PM'];
-    public static $topicLanguageVariants = ['uk' => 'Українська', 'ru' => 'Русский', 'en' => 'English'];
-    public static $topicSectionVariants = [0 => 'First var', 2 => 'Second Variant'];
-    public static $countryVariants = [];
+//    public static $topicSectionVariants = [0 => 'First var', 2 => 'Second Variant'];
+//    public static $countryVariants = [];
+
+    const PARTICIPATION_TYPE_LISTENER = 'listener';
+    const PARTICIPATION_TYPE_SPEAKER = 'speaker';
 
     public static function tableName()
     {
@@ -39,8 +37,7 @@ class Member extends ActiveRecord
     public function init()
     {
         parent::init();
-        $countries = Countries::find()->asArray()->all();
-        self::$countryVariants = ArrayHelper::map($countries, 'alpha_2', 'name');
+        $this->participationType = self::PARTICIPATION_TYPE_LISTENER;
     }
 
 
@@ -63,17 +60,47 @@ class Member extends ActiveRecord
             'organisationDepartment',
             'organisationAddress',
             'organisationActivity',
+            'organisationUrl',
 
             'nameEng', //like in international passport
             'participationType', // listener or speaker
             'topicTitle',
             'topicLanguage',
             'topicSection',
+            'papersCount',
 
             'created_at',
             'updated_at',
         ];
     }
+
+    public function attributeLabels()
+    {
+        return [
+            'country' => Yii::t('app.member', 'Country of residence'),
+            'name' => Yii::t('app.member', 'Full name'),
+            'position' => Yii::t('app.member', 'Position'),
+            'phone' => Yii::t('app.member', 'Phone number'),
+            'email' => Yii::t('app.member', 'E-mail'),
+            'interest' => Yii::t('app.member', 'Research interests'),
+            'scienceDegree' => Yii::t('app.member', 'Scientific Degree'),
+            'scienceTitle' => Yii::t('app.member', 'Academic rank'),
+
+            'organisationTitle' => Yii::t('app.member', 'Full title'),
+            'organisationDepartment' => Yii::t('app.member', 'Name of the department/division'),
+            'organisationAddress' => Yii::t('app.member', 'Mailing address'),
+            'organisationActivity' => Yii::t('app.member', 'Field of activity'),
+            'organisationUrl' => Yii::t('app.member', 'Website'),
+
+            'nameEng' => Yii::t('app.member', 'Name and Surname according to your international passport (for the certificate)'), //like in international passport
+//            'participationType' => Yii::t('app.member', ''), // listener or speaker
+            'topicTitle' => Yii::t('app.member', 'Enter the title of your report'),
+            'topicLanguage' => Yii::t('app.member', 'Language of report'),
+            'topicSection' => Yii::t('app.member', 'Select section'),
+
+        ];
+    }
+
 
     public function rules()
     {
@@ -103,6 +130,7 @@ class Member extends ActiveRecord
                     'topicTitle',
                     'topicLanguage',
                     'topicSection',
+                    'papersCount',
                 ], 'required'],
             [
                 [
@@ -123,6 +151,93 @@ class Member extends ActiveRecord
 
     public function getCountryObj()
     {
-        return $this->hasOne(Countries::className(), ['alpha_2' => 'country' ]);
+        return $this->hasOne(Countries::className(), ['alpha_2' => 'country']);
+    }
+
+    public static function getScienceDegreeVariants()
+    {
+        return [
+            Yii::t('app.member', 'Doctor of Sciences'),
+            Yii::t('app.member', 'Doctor of Technical Sciences'),
+            Yii::t('app.member', 'Doctor of Economic Sciences'),
+            Yii::t('app.member', 'Doctor of Physical and Mathematical Sciences'),
+            Yii::t('app.member', 'Candidate of Sciences (Ph.D.)'),
+            Yii::t('app.member', 'Candidate of Technical Sciences (Ph.D.)'),
+            Yii::t('app.member', 'Candidate of Economic Sciences (Ph.D.)'),
+            Yii::t('app.member', 'Candidate of Physical and Mathematical Sciences (Ph.D.)'),
+            Yii::t('app.member', 'No Degree'),
+        ];
+    }
+
+    public static function getParticipationTypeVariants()
+    {
+        return [
+            self::PARTICIPATION_TYPE_LISTENER => Yii::t('app.member', 'I will participate as <b>Listener</b>'),
+            self::PARTICIPATION_TYPE_SPEAKER => Yii::t('app.member', 'I will participate as <b>Speaker</b>')
+        ];
+    }
+
+    public static function getScienceTitleVariants()
+    {
+        return [
+            Yii::t('app.member', 'Full Professor'),
+            Yii::t('app.member', 'Docent'),
+            Yii::t('app.member', 'No Rank'),
+        ];
+    }
+
+    public static function getOrganisationActivityVariants()
+    {
+        return [
+            Yii::t('app.member', 'Education, Science'),
+            Yii::t('app.member', 'Information Technology'),
+            Yii::t('app.member', 'Banking, Finance, Investment'),
+            Yii::t('app.member', 'Hotel and Restaurant Business, Tourism'),
+            Yii::t('app.member', 'Public Administration'),
+            Yii::t('app.member', 'Consulting, Analytics, Audit'),
+            Yii::t('app.member', 'Culture, Showbiz, Entertainment'),
+            Yii::t('app.member', 'Medicine, Pharmaceuticals, Health Care'),
+            Yii::t('app.member', 'Real Estate'),
+            Yii::t('app.member', 'Non-profit Organizations'),
+            Yii::t('app.member', 'Industry, Manufacturing'),
+            Yii::t('app.member', 'Agriculture, Agribusiness'),
+            Yii::t('app.member', 'Construction, Architecture'),
+            Yii::t('app.member', 'Telecommunications'),
+            Yii::t('app.member', 'Trade, Commerce'),
+            Yii::t('app.member', 'Transport, Auto Business'),
+            Yii::t('app.member', 'Services'),
+        ];
+    }
+
+    public static function getTopicLanguageVariants()
+    {
+        return [
+            Yii::t('app.member', 'English'),
+            Yii::t('app.member', 'Ukrainian'),
+            Yii::t('app.member', 'Russian'),
+        ];
+    }
+
+    public static function getTopicSectionVariants()
+    {
+        return [
+            Yii::t('app.member', 'Plenary Session'),
+            Yii::t('app.member', 'Section 1. Integration of Strategic Management with Portfolio, Program and Project Management: Models, Methods and Techniques'),
+            Yii::t('app.member', 'Section 2. Managing ІТ Projects'),
+            Yii::t('app.member', 'Section 3. Application of Effective Management Technologies in Practice'),
+        ];
+    }
+
+    public static function getCountryVariants()
+    {
+//        $countries = Countries::find()->asArray()->all();
+//        return ArrayHelper::map($countries, 'alpha_2', 'name');
+        return [
+            'ua' => Yii::t('app.member', 'Ukraine'),
+            'ru' => Yii::t('app.member', 'Russia'),
+            'by' => Yii::t('app.member', 'Belarus'),
+            'kz' => Yii::t('app.member', 'Kazakhstan'),
+            'other' => Yii::t('app.member', 'Other'),
+        ];
     }
 }
--- frontend/views/member/_form.php
+++ frontend/views/member/_form.php
@@ -11,7 +11,7 @@ use common\models\Member;
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
-<div class="member-form">
+<div class="member-form registration">
 
     <?php if (count($model->errors) > 0): ?>
         <pre class="text-danger">
@@ -23,38 +23,46 @@ use common\models\Member;
     <?php endif; ?>
 
     <?php $form = ActiveForm::begin([
-        'id' => 'login-form',
+        'id' => 'register-member',
         'options' => ['class' => 'form-horizontal'],
         'fieldConfig' => [
-            'template' => "{label}\n<div class=\"col-lg-4\">{input}</div>\n<div class=\"col-lg-6\">{error}</div>",
+            'template' => "{label}\n<div class=\"col-lg-4\">{input}</div>\n<div class=\"col-lg-4\">{error}</div>",
             'labelOptions' => ['class' => 'col-lg-4 control-label'],
         ],
     ]); ?>
 
-    <?= $form->field($model, 'country')->dropDownList(Member::$countryVariants) ?>
+    <?= $form->field($model, 'country')->dropDownList(Member::getCountryVariants()) ?>
 
-    <h3 class="text-center text-info">Сведения об участнике</h3>
+    <h3 class="text-center text-info middle-header">
+        <?= Yii::t('app.member', 'Information about the participant') ?>
+    </h3>
 
     <?= $form->field($model, 'name') ?>
     <?= $form->field($model, 'position') ?>
     <?= $form->field($model, 'phone') ?>
     <?= $form->field($model, 'email') ?>
     <?= $form->field($model, 'interest') ?>
-    <?= $form->field($model, 'scienceDegree')->dropDownList(Member::$scienceDegreeVariants) ?>
-    <?= $form->field($model, 'scienceTitle')->dropDownList(Member::$scienceTitleVariants) ?>
+    <?= $form->field($model, 'scienceDegree')->dropDownList(Member::getScienceDegreeVariants()) ?>
+    <?= $form->field($model, 'scienceTitle')->dropDownList(Member::getScienceTitleVariants()) ?>
 
-    <h3 class="text-center text-info">Сведения об организации</h3>
+    <h3 class="text-center text-info middle-header">
+        <?= Yii::t('app.member', 'Organisation information') ?>
+    </h3>
 
     <?= $form->field($model, 'organisationTitle') ?>
-    <?= $form->field($model, 'organisationAddress') ?>
-    <?= $form->field($model, 'organisationActivity')->dropDownList(Member::$organisationActivityVariants) ?>
     <?= $form->field($model, 'organisationDepartment') ?>
+    <?= $form->field($model, 'organisationAddress') ?>
+    <?= $form->field($model, 'organisationActivity')->dropDownList(Member::getOrganisationActivityVariants()) ?>
+    <?= $form->field($model, 'organisationUrl') ?>
 
 
-    <h3 class="text-center text-info">Формат участия</h3>
+
+    <h3 class="text-center text-info middle-header">
+        <?= Yii::t('app.member', 'Form of participation') ?>
+    </h3>
 
     <?= $form->field($model, 'nameEng') ?>
-    <?= Html::activeRadioList($model, 'participationType', Member::$participationTypeVariants, [
+    <?= Html::activeRadioList($model, 'participationType', Member::getParticipationTypeVariants(), [
         'item' => function ($index, $label, $name, $checked, $value) {
             if ($index == 0) {
                 return '<div class="col-lg-3 col-lg-offset-3"><label class="radio-inline">' . Html::radio($name, $checked, ['value' => $value]) . $label . '</label></div>';
@@ -62,29 +70,81 @@ use common\models\Member;
             return '<div class="col-lg-6"><label class="radio-inline">' . Html::radio($name, $checked, ['value' => $value]) . $label . '</label></div>';
         }
     ]); ?>
-    <!--    --><? //= $form->field($model, 'participationType')->radioList($model->participation)->label(false) ?>
-    <?= $form->field($model, 'topicTitle') ?>
-    <div class="row">
-        <div class="col-lg-4 col-lg-offset-2">
-            <?= BootHtml::activeDropDownList(
-                $model,
-                'topicLanguage',
-                Member::$topicLanguageVariants,
-                ['class' => 'form-control']
-            ) ?>
-        </div>
-        <div class="col-lg-4">
-            <?= BootHtml::activeDropDownList(
-                $model,
-                'topicSection',
-                Member::$topicSectionVariants,
-                ['class' => 'form-control']
-            ) ?>
+
+    <div class="hidden info-for-speaker">
+        <?= $form->field($model, 'topicTitle', [
+            'inputOptions' => [
+                'placeholder' => $model->getAttributeLabel('topicTitle'),
+                'class' => 'form-control'
+            ],
+            'template' => "\n<div class=\"col-lg-6 col-lg-offset-3\">{input}</div>\n<div class=\"col-lg-3 \">{error}</div>"
+        ]) ?>
+        <div class="row">
+            <div class="col-lg-4 col-lg-offset-2">
+                <?= BootHtml::activeDropDownList(
+                    $model,
+                    'topicLanguage',
+                    Member::getTopicLanguageVariants(),
+                    ['class' => 'form-control']
+                ) ?>
+            </div>
+            <div class="col-lg-4">
+                <?= BootHtml::activeDropDownList(
+                    $model,
+                    'topicSection',
+                    Member::getTopicSectionVariants(),
+                    ['class' => 'form-control']
+                ) ?>
+            </div>
         </div>
     </div>
 
 
-    <h3 class="text-center text-info">К оплате</h3>
+    <h3 class="text-center text-info middle-header">
+        <?= Yii::t('app.member', 'Payment') ?>
+    </h3>
+
+    <table class="table table-bordered summary-info text-center" >
+        <tr class="success">
+            <th><?= Yii::t('app.member', 'Full name') ?></th>
+            <th><?= Yii::t('app.member', 'Registration fee') ?></th>
+            <th class=""><?= Yii::t('app.member', 'Conference papers') ?></th>
+            <th><?= Yii::t('app.member', 'Total to pay') ?></th>
+        </tr>
+        <tr>
+            <td id="table-full-name"></td>
+            <td id="table-registration-fee"></td>
+            <td class=""><?=
+                BootHtml::activeDropDownList(
+                    $model,
+                    'papersCount',
+                    [
+                        'no' => 'No papers',
+                        1 => 1,
+                        2 => 2,
+                        3 => 3,
+                        4 => 4,
+                        5 => 5,
+                        6 => 6,
+                        7 => 7,
+                        8 => 8,
+                        9 => 9,
+                        10 => 10,
+                    ],
+                    ['class' => 'form-control ']
+                )
+                ?>
+                <span id="price-for-papers"></span>
+            </td>
+            <td class="total-sum"></td>
+        </tr>
+    </table>
+    <div class="row">
+        <h2 class="pull-right super-total">
+            <?= Yii::t('app.member', 'Total sum'); ?>
+            <span class="total-sum text-primary"></span>
+        </h2>
+    </div>
 
 
     <div class="form-group">
@@ -94,3 +154,93 @@ use common\models\Member;
     <?php ActiveForm::end(); ?>
 
 </div>
+
+<?php
+$bindFullName = <<< JS
+
+var selectedCountry;
+var registrationFee;
+var priceForPapers;
+
+$('#member-name').on('change', function(){
+    setTableFullName();
+});
+$('#member-country').on('change', function(){
+    setTableRegistrationFee();
+    setPapersPrice();
+    setTotalPrice();
+});
+$('#member-paperscount').on('change', function(){
+    setPapersPrice();
+    setTotalPrice();
+});
+
+$('input[type=radio]', '#register-member').on('change', function(){
+    if($(this).val() == 'speaker'){
+        $('.info-for-speaker').removeClass('hidden');
+    }else{
+        $('.info-for-speaker').addClass('hidden');
+    }
+});
+
+
+function setTableFullName(){
+    var nameField = $('#member-name').val();
+    $('#table-full-name').html(nameField || 'No name');
+}
+setTableFullName();
+
+function setTableRegistrationFee(){
+    selectedCountry = $('#member-country').val();
+    var tableField;
+
+    if(selectedCountry == 'ua'){
+        registrationFee = 350;
+        tableField = registrationFee + ' грн';
+    }else{
+        registrationFee = 100;
+        tableField = '$' + registrationFee;
+    }
+    $('#table-registration-fee').html(tableField);
+}
+setTableRegistrationFee();
+
+function setPapersPrice(){
+    var papersCount = +$('#member-paperscount').val();
+    var priceForPapersLabel;
+
+    if(selectedCountry == 'ua'){
+        priceForPapers = papersCount * 150;
+        priceForPapersLabel = priceForPapers + ' грн';
+    }else{
+        priceForPapers = papersCount * 50;
+        priceForPapersLabel = '$' + priceForPapers;
+    }
+    if(papersCount > 0){
+        $('#price-for-papers').html(priceForPapersLabel);
+    }
+}
+
+function setTotalPrice(){
+    var totalSum = 0;
+
+    if(priceForPapers){
+        totalSum = +registrationFee + +priceForPapers;
+    }else{
+        totalSum = +registrationFee ;
+    }
+
+    if(selectedCountry == 'ua'){
+        $('.total-sum').html(totalSum + ' грн');
+    }else{
+        $('.total-sum').html('$' + totalSum );
+    }
+}
+setTotalPrice();
+
+JS;
+
+
+$this->registerJs($bindFullName);
+
+?>
