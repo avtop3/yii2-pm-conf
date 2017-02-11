52c6c325708bfb1c65a5194dec35a416b65281c0
niki_mikky <niki.mikkym@gmail.com>
2017-02-11 18:50:25 +0200

    fixed timers

--- frontend/views/site/en-US/index.php
+++ frontend/views/site/en-US/index.php
@@ -20,6 +20,10 @@ $this->params['bodyClass'] = 'homepage';
                                     <h1 class="animation animated-item-2">Integrated Strategic Management, <br> Portfolio, Program, and Project Management</h1>
                                 </div>
                                 <div id="sliderCountEN" class="counter"></div>
+                                <div class="counter off animation animated-item-2">
+                                    <p class="in_progress">Conference in progress</p>
+                                    <p class="end">The conference is finished!</p>
+                                </div>
                                 <div class="container text-center action-buttons">
                                     <a href="/en/registration" class="regist">Register now</a>
                                     <a href="/documents/en/information_page.pdf" class="pdf">
@@ -250,7 +254,8 @@ $this->params['bodyClass'] = 'homepage';
                 <p class="descr">Registration deadline in:</p>
             </div>
             <div class="col-md-6">
-                <div id="registrEndEN"></div>
+                <!--<div id="registrEndEN"></div>-->
+                <p class="descr">Registration is over</p>
             </div>
         </div>
         <div class="row wow fadeInDown">
@@ -300,7 +305,8 @@ $this->params['bodyClass'] = 'homepage';
                 <p class="descr">Deadline for paper submissions in:</p>
             </div>
             <div class="col-md-6">
-                <div id="articleEndEN"></div>
+                <!--<div id="articleEndEN"></div>-->
+                <p class="descr">Articles are no longer accepted</p>
             </div>
         </div>
         <div class="row wow fadeInDown">
